fe85a85cea640bcb68d68b65fd00a8ec7be1604a
Tarun Jain <tarunjain3820@gmail.com>
2025-02-12 02:30:32 +0530

    feat: deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    basePath: "/jasper-colin",
+    output: "export",
+    reactStrictMode: true,
   /* config options here */
 };
 
